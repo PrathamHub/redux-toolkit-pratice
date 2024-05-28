c8351291ca739e514e950467b71302e037409258
Pratham <prathambagrecha1430@gmail.com>
2024-05-28 17:41:12 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import "./App.css";
+import ProductList from "./components/ProductList";
+import Cart from "./components/Cart";
+import store from "./components/reducer/store";
+import { Provider } from "react-redux";
+import { useState } from "react";
+
+const App = () => {
+  // const [cart, setCart] = useState([]);
+
+  // const addToCart = (product) => {
+  //   setCart([...cart, product]);
+  // };
+
+  return (
+    <Provider store={store}>
+      <div>
+        <ProductList />
+        <Cart />
+      </div>
+    </Provider>
+  );
+};
+
+export default App;
---
+++ src/components/Cart.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { connect } from "react-redux";
+import { useSelector } from "react-redux";
+const Cart = () => {
+  const cart = useSelector((state) => state.cart);
+  return (
+    <div>
+      <h2>Shopping Cart</h2>
+      <ul>
+        {cart.map((item) => (
+          <li key={item.id}>
+            {item.name} - ${item.price}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+// const mapStateToProps = (state) => ({
+//   cart: state.cart,
+// });
+
+export default Cart;
